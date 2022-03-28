4c21ca5af78a3cf3298352e9aa65f651c9e33186
Janik Burkart <janik.burkart@bluewin.ch>
2022-03-28 22:46:33 +0200

    VL6: RaceHorse

---
+++ src/main/java/ch/hslu/ad/Nebenläufigkeit/Latch/Latch.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2022 Hochschule Luzern - Informatik.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ch.hslu.ad.Nebenläufigkeit.Latch;
+
+/**
+ * Eine Synchronisationshilfe, die es ermöglicht, einen oder mehrere Threads warten zu lassen, bis
+ * diese durch andere Threads aufgeweckt werden. Latches sperren so lange, bis sie einmal ausgelöst
+ * werden. Danach sind sie frei passierbar.
+ */
+public class Latch implements Synch {
+
+    private boolean started = false;
+
+    @Override
+    public synchronized void acquire() throws InterruptedException {
+        while(started){
+            this.wait();
+        }
+    }
+
+    @Override
+    public synchronized void release() {
+        while(!started){
+            started = true;
+            this.notifyAll();
+        }
+    }
+}
